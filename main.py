4cbad5b77d3c0cd7c5815a1c480186ff96749fe2
Russkih Anatoly <titanofdarkness@gmail.com>
2020-12-11 09:41:20 +1000

    Prototype.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from json import loads
 from os import getenv
-from typing import NamedTuple
+from typing import NamedTuple, List
 
 import requests
 
@@ -16,6 +16,29 @@ class ApiError(TypeError):
 class FlaskAPIToken(NamedTuple):
     token: str
     expired: datetime
+
+
+class FlaskAPIIDPSelf(NamedTuple):
+    id: int
+    first_name: str
+    last_name: str
+    edc_username: str
+    email: str
+    finished_tutorial: bool
+    company_name: str
+    study_subject_label: str
+
+
+class FlaskAPISubjectStudies(NamedTuple):
+    id: int
+    study_id: int
+    official_title: str
+    brief_title: str
+    detailed_description: str
+    sponsor: str
+    url: str
+    unique_protocolid: str
+    dbname: str
 
 
 class FlaskAPI:
@@ -43,6 +66,56 @@ class FlaskAPI:
         else:
             raise ApiError('Invalid api response')
 
+    def subject_studies(self, token: str, subject_id: int) -> List[FlaskAPISubjectStudies]:
+        full_url = f'{self.base_url}/flask/study/subject-studies'
+        headers = {
+            'Authorization': f'JWT {token}'  # or 'Bearer <ACCESS_TOKEN>'
+        }
+        data = {
+            "subject_id": subject_id
+        }
+        res = requests.post(full_url, json=data, timeout=DEFAULT_NET_DELAY, headers=headers)
+        if res.status_code == 200:
+            text = res.text
+            json = loads(text)
+            result: List[FlaskAPISubjectStudies] = []
+            for item in json:
+                result.append(FlaskAPISubjectStudies(
+                    item['id'],
+                    item['study_id'],
+                    item['official_title'],
+                    item['brief_title'],
+                    item['detailed_description'],
+                    item['sponsor'],
+                    item['url'],
+                    item['unique_protocolid'],
+                    item['dbname'],
+                ))
+            return result
+        else:
+            raise ApiError('Invalid api response')
+
+    def create_crf_and_insert_data(self, token):
+        full_url = f'{self.base_url}/flask/crf/create-CRF-and-insert-data'
+        headers = {
+            'Authorization': f'JWT {token}'  # or 'Bearer <ACCESS_TOKEN>'
+        }
+        data = {
+            "study_id": 3581600,
+            "subject_label": "001 - 023",
+            "event_name": "01-Screening",
+            "crf_name": "Demographics",
+            "crf_data": {
+                "GENDER": "Female",
+                "AGE": "21"
+            }
+        }
+        res = requests.post(full_url, json=data, timeout=DEFAULT_NET_DELAY, headers=headers)
+        if res.status_code == 200:
+            return
+        else:
+            raise ApiError('Invalid api response')
+
 
 class FlaskAPIIDP(FlaskAPI):
     def __init__(self, username: str, password: str):
@@ -52,12 +125,34 @@ class FlaskAPIIDP(FlaskAPI):
     def authorize(self):
         full_url = f'{self.base_url}/auth/mobile-form-authorization'
         data = {
-            "email": self.username,
-            "password": self.password
+            "email": 'subjectdemo@repnets.com',
+            "password": '12345678De'
         }
         res = requests.post(full_url, json=data, timeout=DEFAULT_NET_DELAY)
         if res.status_code == 200:
             pass
+        else:
+            raise ApiError(f'Invalid api response ({res.status_code}): {res.text}')
+
+    def get_self(self, token: str) -> FlaskAPIIDPSelf:
+        headers = {
+            'Authorization': f'JWT {token}'  # or 'Bearer <ACCESS_TOKEN>'
+        }
+        full_url = f'{self.base_url}/auth/self'
+        res = requests.get(full_url, headers=headers)
+        if res.status_code == 200:
+            text = res.text
+            json = loads(text)
+            return FlaskAPIIDPSelf(
+                json['id'],
+                json['first_name'],
+                json['last_name'],
+                json['edc_username'],
+                json['email'],
+                json['finished_tutorial'],
+                json['company_name'],
+                json['study_subject_label'],
+            )
         else:
             raise ApiError(f'Invalid api response ({res.status_code}): {res.text}')
 
